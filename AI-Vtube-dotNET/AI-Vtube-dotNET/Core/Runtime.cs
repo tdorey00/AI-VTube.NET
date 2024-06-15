0da3a94d45954d280ee038ef6e1d51f20e9caed2
tdorey00 <tdorey00@gmail.com>
2024-06-15 15:05:12 -0400

    Initial Commit

---
+++ AI-Vtube-dotNET/AI-Vtube-dotNET/Core/Runtime.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace AI_Vtube_dotNET.Core
+{
+    public class Runtime
+    {
+        private readonly ILogger _logger;
+        public Runtime(ILogger<Runtime> logger) 
+        { 
+            _logger = logger;
+        }
+
+        //This becomes async one day
+        public void RunAsync()
+        {
+            _logger.LogInformation("Hi");
+        }
+    }
+}
